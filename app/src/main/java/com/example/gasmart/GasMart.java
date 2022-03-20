c6c553b63c8cf6e505afcf965419b5e089f272a5
vaish0110 <79245967+vaish0110@users.noreply.github.com>
2022-03-20 17:14:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gasmart/GasMart.java
@@ -0,0 +1,14 @@
+package com.example.gasmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GasMart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
