7fcd3fe5428929189dc6dec5c1d43882d04165e9
vaish0110 <79245967+vaish0110@users.noreply.github.com>
2022-05-07 23:38:46 +0530

    modified

--- app/src/main/java/com/example/gasmart/login.java
+++ app/src/main/java/com/example/gasmart/login.java
@@ -20,75 +20,72 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class login extends AppCompatActivity {
-   EditText le;
-   EditText pw;
-   TextView fpw;
-   Button login;
+    EditText le;
+    EditText pw;
+    TextView fpw;
+    Button login;
     FirebaseAuth fAuth;
-   Button re;
+    Button re;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-          Login();
-          Register();
-          ForgotPassword();
-    }
-void Login(){
-    le=findViewById(R.id.LogInEmail);
-    pw=findViewById(R.id.LogInPassword);
-    login=(Button) findViewById(R.id.login);
-    fAuth = FirebaseAuth.getInstance();
 
-    login.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            String Email= le.getText().toString().trim();
-            String password= pw.getText().toString().trim();
-            if(TextUtils.isEmpty(Email)){
-                le.setError("Email is Required");
-                return;
-            }
-            if(TextUtils.isEmpty(Email)){
-                pw.setError("Password is Required");
-                return;
-            }
-            fAuth.signInWithEmailAndPassword(Email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
-                        Toast.makeText(login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(), dashboard.class));
-                    }else {
-                        Toast.makeText(login.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(), dashboard.class));
-                    }
+
+        le = findViewById(R.id.LogInEmail);
+        pw = findViewById(R.id.LogInPassword);
+        login = (Button) findViewById(R.id.login);
+        fAuth = FirebaseAuth.getInstance();
+        fpw = findViewById(R.id.ForgotPassword);
+        re = (Button) findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = le.getText().toString().trim();
+                String password = pw.getText().toString().trim();
+                if (TextUtils.isEmpty(Email)) {
+                    le.setError("Email is Required");
+                    return;
                 }
-            });
+                if (TextUtils.isEmpty(Email)) {
+                    pw.setError("Password is Required");
+                    return;
+                }
+                fAuth.signInWithEmailAndPassword(Email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), dashboard.class));
+                        } else {
+                            Toast.makeText(login.this, "Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), dashboard.class));
+                        }
+                    }
+                });
 
-        }
-    });
+            }
+        });
 
-}
-void Register(){
-    re=(Button) findViewById(R.id.register);
-    re.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent= new Intent(login.this,register.class);
-            startActivity(intent);
-        }
-    });
-}
-void ForgotPassword(){
-    fpw=findViewById(R.id.ForgotPassword);
-    fpw.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent= new Intent(login.this,forgotpassword.class);
-            startActivity(intent);
-        }
-    });
-}
+
+        re.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, register.class);
+                startActivity(intent);
+            }
+        });
+
+
+        fpw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, forgotpassword.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
