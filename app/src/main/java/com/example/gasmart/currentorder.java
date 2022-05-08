834c12d4bf521bb6d2ebd45de22943b80b5f0d31
vaish0110 <79245967+vaish0110@users.noreply.github.com>
2022-05-08 19:02:13 +0530

    order change

--- app/src/main/java/com/example/gasmart/currentorder.java
+++ app/src/main/java/com/example/gasmart/currentorder.java
@@ -13,7 +13,7 @@ public class currentorder extends AppCompatActivity {
     ImageView arrow,edit;
     Button cancelOrder;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // oncreate
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currentorder);
 
